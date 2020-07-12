c4eee24d044bfd5c48a85538a664825aa1ce027d
Jon Shamir <jon.shamir@gmail.com>
2020-07-12 17:28:20 +0300

    Project filtering working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,70 @@
 const menuScrollThreshold = 150;
-let floatingNav;
+let floatingNavEl;
 
 window.addEventListener("load", onLoad);
 window.onscroll = onScroll;
+// document.addEventListener('DOMContentLoaded', onLoad);
 
 function onLoad() {
-  floatingNav = document.getElementById("floating-nav");
+  floatingNavEl = document.getElementById("floating-nav");
   onScroll();
 
   setupShuffle();
 }
 
-function setupShuffle() {
-  var shuffleGrid = document.getElementById("shuffle-grid");
-  if (shuffleGrid) {
-    window.shuffleInstance = new window.Shuffle(shuffleGrid, {
-      itemSelector: ".grid-brick",
-      sizer: ".sizer-element",
-    });
+function onScroll() {
+  if (window.hideNav && floatingNavEl) {
+    if (document.documentElement.scrollTop > menuScrollThreshold)
+      floatingNavEl.classList.remove("hidden");
+    else floatingNavEl.classList.add("hidden");
   }
 }
 
-function onScroll() {
-  if (window.hideNav && floatingNav) {
-    if (document.documentElement.scrollTop > menuScrollThreshold)
-      floatingNav.classList.remove("hidden");
-    else floatingNav.classList.add("hidden");
+function setupShuffle() {
+  var shuffleGrid = document.getElementById("shuffle-grid");
+  if (shuffleGrid) {
+    window.shuffle = new window.Shuffle(shuffleGrid, {
+      itemSelector: ".grid-brick",
+      sizer: ".sizer-element",
+      isCentered: true,
+    });
+  }
+  setupFilterButtons();
+}
+
+function setupFilterButtons() {
+  const options = document.querySelector(".filter-options");
+
+  const filterButtons = Array.from(options.children);
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", handleFilterClick, false);
+  });
+}
+
+function handleFilterClick(e) {
+  const btn = e.currentTarget;
+  const isActive = btn.classList.contains("active");
+  const btnGroup = btn.getAttribute("data-group");
+
+  removeActiveClassFromChildren(btn.parentNode);
+
+  let filterGroup;
+  if (isActive) {
+    btn.classList.remove("active");
+    filterGroup = Shuffle.ALL_ITEMS;
+  } else {
+    btn.classList.add("active");
+    filterGroup = btnGroup;
+  }
+
+  console.log(filterGroup);
+
+  window.shuffle.filter(filterGroup);
+}
+
+function removeActiveClassFromChildren(parent) {
+  const { children } = parent;
+  for (let i = children.length - 1; i >= 0; i--) {
+    children[i].classList.remove("active");
   }
 }
