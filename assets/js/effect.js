b586c7f8f87a962fa61aed1f9bd3459fe9f39f22
Jon Shamir <jon.shamir@gmail.com>
2021-07-25 00:17:06 +0300

    CMYK homepage effect

--- assets/js/effect.js
+++ assets/js/effect.js
@@ -9,103 +9,156 @@ window.addEventListener("load", function () {
 });
 
 const vertex = `
-void main() {
-    gl_Position = vec4( position, 1.0 );
+// attribute vec3 position;
+varying vec2 vUv;
+
+void main () {
+  gl_Position = vec4(position.xyz, 1.0);
+  vUv = gl_Position.xy * 0.5 + 0.5;
 }
 `;
 
 const fragment = `
-#extension GL_OES_standard_derivatives: enable
+precision highp float;
+#define GLSLIFY 1
 
-uniform vec2 u_resolution;
-uniform vec2 u_mouse;
+vec3 mod289(vec3 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x)
+{
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+  return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// Classic Perlin noise
+float cnoise(vec3 P)
+{
+  vec3 Pi0 = floor(P); // Integer part for indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+
 uniform float u_time;
-uniform float u_pixel_ratio;
+uniform vec2 u_resolution;
+varying vec2 vUv; // Texcoords
 
-vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+float getFadingNoise(vec2 uv, vec2 offset) {
+  float noiseScale = 6.0;
 
-float snoise(vec2 v) {
-    const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
-                        0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
-                        -0.577350269189626,  // -1.0 + 2.0 * C.x
-                        0.024390243902439); // 1.0 / 41.0
-    vec2 i  = floor(v + dot(v, C.yy) );
-    vec2 x0 = v -   i + dot(i, C.xx);
-    vec2 i1;
-    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-    vec4 x12 = x0.xyxy + C.xxzz;
-    x12.xy -= i1;
-    i = mod289(i); // Avoid truncation effects in permutation
-    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
-        + i.x + vec3(0.0, i1.x, 1.0 ));
-
-    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
-    m = m*m ;
-    m = m*m ;
-    vec3 x = 2.0 * fract(p * C.www) - 1.0;
-    vec3 h = abs(x) - 0.5;
-    vec3 ox = floor(x + 0.5);
-    vec3 a0 = x - ox;
-    m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
-    vec3 g;
-    g.x  = a0.x  * x0.x  + h.x  * x0.y;
-    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-    return 130.0 * dot(m, g);
+  float n = cnoise(vec3(uv*noiseScale + offset, u_time * 0.2))*0.5;
+  n = n + 2.0 - abs(uv.x-0.5) - sqrt(sqrt(uv.y));
+  return clamp(n, 0.0, 1.0);
 }
 
-vec3 contours(float x, float levels) {
-  float f = fract(x * levels);
-  float df = fwidth(x * levels);
+vec2 getResponsiveUV() {
+  vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+  uv.x *= u_resolution.x / u_resolution.y;
 
-  float y = smoothstep(df*2.0, df*4.0, f) + smoothstep(df*0.5, df*-0.0, f);
-
-  y = y * 0.15 + 0.85;
-  return vec3(y);
+  return uv;
 }
 
-vec3 contour_fill(float x, float levels) {
-  float c = floor(x*levels) / levels;
-  vec3 color = vec3(.08 - c*0.08, pow(c, 3.0), .1 + c*.9);
-  // c = c*0.6;
-  // vec3 color = vec3(c, c, c);
+float getHalftone(float value, mat2 rotation) {
+  float numDots = 10.0;
 
-  color = color * 0.9 + 0.1;
-  return color;
+  vec2 st = rotation * getResponsiveUV();
+  vec2 nearest = 2.0*fract(numDots * st) - 1.0;
+  float dist = length(nearest);
+  float radius = sqrt(1.0-value) - 0.3;
+  vec3 white = vec3(1.0, 1.0, 1.0);
+  vec3 black = vec3(0.0, 0.0, 0.0);
+  return step(radius, dist);
 }
 
-void main() {
-    vec2 mouse = vec2(u_mouse.x/u_resolution.x, 1.0 - u_mouse.y/u_resolution.y)*u_pixel_ratio;
-    mouse.x *= u_resolution.x/u_resolution.y;
-    vec2 st = gl_FragCoord.xy/u_resolution.xy;
-    st.x *= u_resolution.x/u_resolution.y;
-    vec3 color = vec3(0.0);
-    vec2 pos = vec2(st*(2.0/u_pixel_ratio));
+void main () {
+  float nC = getFadingNoise(vUv, vec2(0,0));
+  float nM = getFadingNoise(vUv, vec2(0,1.5));
+  float nY = getFadingNoise(vUv, vec2(1.5,0));
 
-    float DF = 0.0;
+  float c = getHalftone(nC, mat2(0.707, -0.707, 0.707, 0.707));
+  float m = getHalftone(nM, mat2(0.966, -0.259, 0.259, 0.966));
+  float y = getHalftone(nY, mat2(0.966, 0.259, -0.259, 0.966));
 
-    // Add a random position
-    float a = 0.0;
-    vec2 vel = vec2(u_time*.05);
-    DF += snoise(pos+vel)*.25+.25;
-
-    // Add a random position
-    a = snoise(pos*vec2(cos(u_time*0.05),sin(u_time*0.05))*0.1)*3.1415;
-    vel = vec2(cos(a),sin(a));
-    DF += snoise(pos+vel)*0.25+ 0.25;
-
-    float x = fract(DF);
-
-    // Mouse warp
-    // x -= 0.2*(1.0 - clamp(0.0, 1.0, distance(st, mouse)));
-
-    // Turn to contours
-    color = contour_fill(x, 8.0);
-
-    gl_FragColor = vec4(color, 1.0);
-}
-`;
+  gl_FragColor = vec4(c, m, y, 1.0);
+  // gl_FragColor = vec4(nC, nM, nY, 1.0);
+}`;
 
 function init() {
   container = document.getElementById("hero");
@@ -149,7 +202,6 @@ function init() {
 
 function onWindowResize(event) {
   const w = container.offsetWidth;
-  console.log(w);
   // const margin = parseInt(window.getComputedStyle(container).marginBottom);
   const margin = container.offsetTop;
   const h = container.offsetHeight + margin;
